675dac22b362deebf66786f1c45acaa03c57e800
Gaweł Bartosik <gawel.bartosik@aptiv.com>
2026-07-07 22:20:54 +0200

    Change the ESPHome component to TX only, as RX is pointless here

--- components/kenwood_xs/__init__.py
+++ components/kenwood_xs/__init__.py
@@ -1,18 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, text_sensor
 from esphome.const import (
     CONF_ID,
     ENTITY_CATEGORY_DIAGNOSTIC,
 )
 
 CODEOWNERS = ["@gaweueu"]
-AUTO_LOAD = ["sensor", "text_sensor"]
 
-CONF_BIDIRECTIONAL = "bidirectional"
 CONF_CTRL_PIN = "ctrl_pin"
-CONF_LAST_COMMAND = "last_command"
-CONF_LAST_COMMAND_CODE = "last_command_code"
 CONF_SDAT_PIN = "sdat_pin"
 
 kenwood_xs_ns = cg.esphome_ns.namespace("kenwood_xs")
@@ -23,15 +18,6 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(KenwoodXSComponent),
         cv.Required(CONF_CTRL_PIN): cv.int_range(min=0, max=48),
         cv.Required(CONF_SDAT_PIN): cv.int_range(min=0, max=48),
-        cv.Optional(CONF_BIDIRECTIONAL, default=True): cv.boolean,
-        cv.Optional(CONF_LAST_COMMAND): text_sensor.text_sensor_schema(
-            icon="mdi:remote",
-            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-        ),
-        cv.Optional(CONF_LAST_COMMAND_CODE): sensor.sensor_schema(
-            accuracy_decimals=0,
-            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-        ),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -39,15 +25,5 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], config[CONF_CTRL_PIN], config[CONF_SDAT_PIN])
     await cg.register_component(var, config)
-
-    cg.add(var.set_bidirectional(config[CONF_BIDIRECTIONAL]))
-
-    if CONF_LAST_COMMAND in config:
-        sens = await text_sensor.new_text_sensor(config[CONF_LAST_COMMAND])
-        cg.add(var.set_last_command_text_sensor(sens))
-
-    if CONF_LAST_COMMAND_CODE in config:
-        sens = await sensor.new_sensor(config[CONF_LAST_COMMAND_CODE])
-        cg.add(var.set_last_command_sensor(sens))
 
     cg.add_library("https://github.com/gaweueu/KenwoodXS.git", None)
--- components/kenwood_xs/kenwood_xs.h
+++ components/kenwood_xs/kenwood_xs.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <KenwoodXS.h>
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/core/component.h"
 
 namespace esphome {
@@ -13,25 +11,15 @@ class KenwoodXSComponent : public Component {
   KenwoodXSComponent(uint8_t ctrl_pin, uint8_t sdat_pin);
 
   void setup() override;
-  void loop() override;
   void dump_config() override;
-
-  void set_bidirectional(bool bidirectional) { this->bidirectional_ = bidirectional; }
-  void set_last_command_sensor(sensor::Sensor *sensor) { this->last_command_sensor_ = sensor; }
-  void set_last_command_text_sensor(text_sensor::TextSensor *sensor) { this->last_command_text_sensor_ = sensor; }
 
   void send_command(uint8_t command);
 
  protected:
-  const char *command_name_(uint8_t command) const;
-  void publish_received_command_(uint8_t command);
 
   uint8_t ctrl_pin_;
   uint8_t sdat_pin_;
-  bool bidirectional_{true};
   KenwoodXS kenwood_;
-  sensor::Sensor *last_command_sensor_{nullptr};
-  text_sensor::TextSensor *last_command_text_sensor_{nullptr};
 };
 
 }  // namespace kenwood_xs
